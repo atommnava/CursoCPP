22f6ba72619ca696a7dfce2cabc78ae45784f157
atommnava <atommunoznava@gmail.com>
2026-04-04 17:03:48 -0600

    Constantes

--- .vscode/variables/constantes.cpp
+++ .vscode/variables/constantes.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
 int main(){
+    const double PI = 3.14159;
+    const int vel_luz = 299792;
+    double radio = 5432;
+    double circunferencia = 2 * PI * radio;
+
+    std::cout << circunferencia << " m\n";
+    std::cout << vel_luz << " m/s";
     return 0;
 }
