9e33691dd198b311cd6a3cea3e063593c61152cc
atommnava <atommunoznava@gmail.com>
2026-04-04 16:57:25 -0600

    Variables

---
+++ .vscode/variables/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    int x, y;
+    x = 5;
+    y = 6;
+    int z = x + y;
+    std::cout << z;
+
+    char calificacion = 'A';
+    char inicial = 'B';
+    std::cout << calificacion;
+    std::cout << inicial;
+    return 0;
+}
