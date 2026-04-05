de583fa3f7b1914fde1afa25c2c448cf4d0128d3
atommnava <atommunoznava@gmail.com>
2026-04-05 17:54:42 -0600

    Input

---
+++ input/main.cpp
@@ -0,0 +1,11 @@
+#include "iostream"
+
+using namespace std;
+
+int main(){
+    string nombre;
+    cout << "Escribe tu nombre: ";
+    cin >> nombre;
+    cout << nombre << endl;
+    return 0;
+}
