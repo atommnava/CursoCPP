5a6137838e612f655ae3f595ca1b17dc23236219
atommnava <atommunoznava@gmail.com>
2026-04-05 17:59:56 -0600

    Input

--- input/main.cpp
+++ input/main.cpp
@@ -4,8 +4,12 @@ using namespace std;
 
 int main(){
     string nombre;
+    int edad;
     cout << "Escribe tu nombre: ";
-    cin >> nombre;
-    cout << nombre << endl;
+    getline(cin, nombre);
+
+    cout << "Ahora tu edad: ";
+    cin >> edad;
+    cout << nombre << " y tienes " << edad << " años" <<endl;
     return 0;
 }
