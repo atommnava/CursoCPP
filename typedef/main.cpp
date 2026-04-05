73feefff80c228ae4dd91b83ca83d1d58d2069b3
atommnava <atommunoznava@gmail.com>
2026-04-05 17:30:37 -0600

    Namespace

--- typedef/main.cpp
+++ typedef/main.cpp
@@ -2,11 +2,16 @@
 #include <vector>
 
 typedef std::vector<std::pair<std::string, int>> pair_list_t;
-typedef std::string text_t;
+//typedef std::string text_t;
+//typedef int numero_t;
+
+using text_t = std::string;
+using numero_t = int;
 
 int main(){
     pair_list_t pair_list;
     text_t nombre = "Atom";
+    numero_t edad = 21;
 
-    std::cout << nombre << std::endl;
+    std::cout << nombre << " " << edad << std::endl;
 }
