3896a5ab639ead7d872386e11e299240986dd426
atommnava <atommunoznava@gmail.com>
2026-04-05 17:27:44 -0600

    Typedef

---
+++ typedef/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+typedef std::vector<std::pair<std::string, int>> pair_list_t;
+typedef std::string text_t;
+
+int main(){
+    pair_list_t pair_list;
+    text_t nombre = "Atom";
+
+    std::cout << nombre << std::endl;
+}
