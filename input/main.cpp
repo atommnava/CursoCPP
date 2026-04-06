af445d56a1fb6c6f7cb6d14070f0ffbbbe6fd7d4
atommnava <atommunoznava@gmail.com>
2026-04-05 18:06:16 -0600

    Input

--- input/main.cpp
+++ input/main.cpp
@@ -5,12 +5,13 @@ using namespace std;
 int main(){
     string nombre;
     int edad;
+     cout << "Ahora tu edad: ";
+    cin >> edad;
+
     cout << "Escribe tu nombre: ";
     // Eliminar caracteres de nueva línea o cualquier espacio en blanco
     getline(cin >> ws, nombre);
-
-    cout << "Ahora tu edad: ";
-    cin >> edad;
+   
     cout << nombre << " y tienes " << edad << " años" <<endl;
     return 0;
 }
